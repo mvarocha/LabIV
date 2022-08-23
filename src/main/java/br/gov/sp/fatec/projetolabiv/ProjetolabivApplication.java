7f85ef42f1ae04bf6df1414d9e17e022c72a53d9
Marcus Rocha <marcus.varocha@gmail.com>
2022-08-23 19:19:55 -0300

    project

---
+++ src/main/java/br/gov/sp/fatec/projetolabiv/ProjetolabivApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.projetolabiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetolabivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetolabivApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/projetolabiv/ProjetolabivApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.projetolabiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetolabivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
